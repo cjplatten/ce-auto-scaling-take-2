e2ae96668c97447d8405c31360fc554314d9f5cc
Emily Bennett <emilybennett93@Hotmail.com>
2023-10-23 13:59:00 +0100

    feat: adding security module to root

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,5 +4,11 @@ module "vpc" {
   source          = "./modules/networking"
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
+  azs             = var.azs
+}
 
+module "security" {
+  source = "./modules/security"
+  ssh_ip = var.ssh_ip
+  vpc_id = module.vpc.vpc_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "private_subnets" {
   type        = list(string)
   description = "A list of the CIDR ranges required for the private subnets"
 }
+
+variable "azs" {
+  type        = list(string)
+  description = "A list of the Availability Zones you wish to provision infrastructure in"
+}
+
+variable "ssh_ip" {
+  type        = string
+  description = "The IP address of the computer you wish SSH from"
+}
