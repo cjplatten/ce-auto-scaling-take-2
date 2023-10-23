ff9709a335237e24944ca132109e368c605088a1
Emily Bennett <emilybennett93@Hotmail.com>
2023-10-23 15:15:23 +0100

    refactor: into  ec2 block

--- terraform/modules/app-servers/main.tf
+++ terraform/modules/app-servers/main.tf
@@ -1,15 +1,4 @@
-
-resource "aws_instance" "app-servers" {
-  count         = var.min_ec2_count
-  instance_type = var.instance_type
-  subnet_id     = var.public_subnets[count.index]
-
-  launch_template {
-    id = aws_launch_template.server.id
-  }
-}
-
-
+// data blocks will make a request for information that we may not have already or sometimes changes
 data "aws_ami" "ubuntu" {
   // Gets the latest ubuntu AMI ID
   most_recent = true
@@ -27,9 +16,16 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical's ID - creators of the Ubuntu AMI
 }
 
-resource "aws_launch_template" "server" {
-  image_id             = data.aws_ami.ubuntu.id
-  security_group_names = var.security_group_names
+resource "aws_instance" "app-servers" {
+  count = var.min_ec2_count
+
+  instance_type   = var.instance_type
+  subnet_id       = var.public_subnets[count.index]
+  security_groups = var.security_group_names
+  ami             = data.aws_ami.ubuntu.id
 
 }
+
+
+
 
