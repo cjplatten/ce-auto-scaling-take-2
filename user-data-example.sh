4fb721bc5907d93d331e7477657f89ca3e348ef4
Emily Bennett <emilybennett93@Hotmail.com>
2023-10-24 12:27:23 +0100

    fix: making userdata more obvious

--- user-data-example.sh
+++ user-data-example.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "i'm about to be written to a file" > file.txt
-cat file.txt 
-# "i'm about to be written to a file"
+ls
+cd ce-load-balancing-api/app
+npm install 
+
+# What commands do you need to run to get the application running
+# Think as though your computer had just been turned on
