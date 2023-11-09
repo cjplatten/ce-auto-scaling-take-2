24b76537ec7d56bfe49e0e5673828a7081e8559a
Emily Bennett <emilybennett93@Hotmail.com>
2023-11-09 10:37:01 +0000

    feat: adding dns hostnames

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/20"
-
+  cidr_block           = "10.0.0.0/20"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "ce-auto-scaling"
@@ -28,3 +28,21 @@ resource "aws_subnet" "private_subnets" {
 
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_routes" {
+  count          = length(aws_subnet.public_subnets[*])
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+}
